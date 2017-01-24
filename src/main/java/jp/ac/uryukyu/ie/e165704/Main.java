ad9e53c4ee1f32a0318d300c41523f1e01830142
e165704 <e165704@ie.u-ryukyu.ac.jp>
2017-01-24 10:20:05 +0900

    マップ内容まで

---
+++ src/main/java/jp/ac/uryukyu/ie/e165704/Main.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e165704;
+
+/**
+ * Created by e165704 on 2017/01/24.
+ */
+public class Main extends Mapdata {
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e165704/Map.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e165704;
+
+/**
+ * Created by e165704 on 2017/01/24.
+ */
+public class Map {
+        public int width; //迷路の横の大きさ
+        public int height; //迷路の縦の大きさ
+        public  byte[] data;//実際の迷路データ
+
+        public static final byte PATH = 0; //通路
+        public static final byte WALL = 1; //壁
+        public static final byte ENTRANCE = 2; //入り口
+        public static final byte EXIT = 3; //出口
+    }
+
