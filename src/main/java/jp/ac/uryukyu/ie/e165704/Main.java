33e581a8a050dda75f9a6b5376407530828c1f06
e165704 <e165704@ie.u-ryukyu.ac.jp>
2017-01-27 14:59:28 +0900

    マップ実装前

--- src/main/java/jp/ac/uryukyu/ie/e165704/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165704/Main.java
@@ -3,5 +3,10 @@ package jp.ac.uryukyu.ie.e165704;
 /**
  * Created by e165704 on 2017/01/24.
  */
-public class Main extends Mapdata {
+public class Main{
+    public static void main(String arg[]) {
+
+        Map map = new Map();
+        map.PrintMap();
+    }
 }
--- src/main/java/jp/ac/uryukyu/ie/e165704/Map.java
+++ src/main/java/jp/ac/uryukyu/ie/e165704/Map.java
@@ -4,13 +4,36 @@ package jp.ac.uryukyu.ie.e165704;
  * Created by e165704 on 2017/01/24.
  */
 public class Map {
-        public int width; //迷路の横の大きさ
-        public int height; //迷路の縦の大きさ
-        public  byte[] data;//実際の迷路データ
+        int width; //迷路の横の大きさ
+        int height; //迷路の縦の大きさ
+        int[][] data;//実際の迷路データ
 
         public static final byte PATH = 0; //通路
         public static final byte WALL = 1; //壁
         public static final byte ENTRANCE = 2; //入り口
         public static final byte EXIT = 3; //出口
-    }
+
+        public Map(){
+                width = 9; //迷路の横幅は９
+                height = 6; //迷路の縦幅は６
+                data = new int[height][];
+                data[0] = new int[]{2, 1, 0, 1, 0, 1, 0, 0, 0};  // 0 は 通路
+                data[1] = new int[]{0, 1, 0, 0, 0, 0, 0, 1, 0};  // 1 は 壁
+                data[2] = new int[]{0, 1, 0, 1, 0, 1, 0, 1, 0};  // 2 は 入口
+                data[3] = new int[]{0, 0, 0, 1, 0, 1, 0, 1, 0};  // 3 は 出口
+                data[4] = new int[]{0, 1, 1, 1, 0, 1, 0, 1, 0};
+                data[5] = new int[]{0, 0, 0, 0, 1, 0, 0, 1, 3};
+
+        }
+
+        public void PrintMap(){
+                int i, j;
+                for(i=0; i<height; i++){
+                        for(j=0; j<width; j++){
+                                System.out.print(data[i][j]);
+                        }
+                        System.out.println();
+                }
+        }
+}
 
