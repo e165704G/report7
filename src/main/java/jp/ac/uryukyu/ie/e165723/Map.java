b84e7231636bce5b4db28b8fb222c9b68acd780b
e165704 <e165704@ie.u-ryukyu.ac.jp>
2017-01-29 16:27:45 +0900

    マップ実装完了

--- src/main/java/jp/ac/uryukyu/ie/e165723/Map.java
+++ src/main/java/jp/ac/uryukyu/ie/e165723/Map.java
@@ -14,23 +14,39 @@ public class Map {
         public static final byte EXIT = 3; //出口
 
         public Map(){
-                width = 9; //迷路の横幅は９
-                height = 6; //迷路の縦幅は６
+                width = 11; //迷路の横幅は11
+                height = 8; //迷路の縦幅は8
                 data = new int[height][];
-                data[0] = new int[]{2, 1, 0, 1, 0, 1, 0, 0, 0};  // 0 は 通路
-                data[1] = new int[]{0, 1, 0, 0, 0, 0, 0, 1, 0};  // 1 は 壁
-                data[2] = new int[]{0, 1, 0, 1, 0, 1, 0, 1, 0};  // 2 は 入口
-                data[3] = new int[]{0, 0, 0, 1, 0, 1, 0, 1, 0};  // 3 は 出口
-                data[4] = new int[]{0, 1, 1, 1, 0, 1, 0, 1, 0};
-                data[5] = new int[]{0, 0, 0, 0, 1, 0, 0, 1, 3};
-
+                data[0] = new int[]{1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+                data[1] = new int[]{1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1};  // 0 は 通路
+                data[2] = new int[]{1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1};  // 1 は 壁
+                data[3] = new int[]{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};  // 2 は 入口
+                data[4] = new int[]{1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1};  // 3 は 出口
+                data[5] = new int[]{1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1};
+                data[6] = new int[]{1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1};
+                data[7] = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1};
         }
 
         public void PrintMap(){
                 int i, j;
                 for(i=0; i<height; i++){
                         for(j=0; j<width; j++){
-                                System.out.print(data[i][j]);
+                                //System.out.print(data[i][j]);
+                                switch (data[i][j]) {
+                                        case PATH:
+                                                System.out.print("　");
+                                                break;
+                                        case WALL:
+                                                System.out.print("＃");
+                                                break;
+                                        case ENTRANCE:
+                                                System.out.print("Ｓ");
+                                                break;
+                                        case EXIT:
+                                                System.out.print("Ｇ");
+                                                break;
+
+                                }
                         }
                         System.out.println();
                 }
---
+++ src/main/java/jp/ac/uryukyu/ie/e165723/Player.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e165723;
+
+/**
+ * Created by e165704 on 2017/01/28.
+ */
+public class Player {
+}
