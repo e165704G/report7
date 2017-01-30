09e72c30fd091a2377bd3e773f30b6a7e33a47c9
e165704 <e165704@ie.u-ryukyu.ac.jp>
2017-01-30 23:03:47 +0900

    巨人プレイヤー 人間プレイやーの完成 実際に動かしゴールできる

--- src/main/java/jp/ac/uryukyu/ie/e165723/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165723/Main.java
@@ -1,16 +1,35 @@
 package jp.ac.uryukyu.ie.e165723;
 
-/**
- * Created by e165704 on 2017/01/24.
- */
+import java.io.IOException;
+
 public class Main{
-    public static void main(String arg[]) {
+
+    public static void main(String arg[]) throws IOException {
 
         Map map = new Map();
         map.PrintMap();
-        Player1 player1 = new Player1();
-        player1.PrintPlayer();
-//スタートに当たる位置がある場合にPを表示させたい
+        Player1 player1 = new Player1(map);
+
+
+        if(player1.wallbreak == false){
+            System.out.print("あなたは迷える子ひつじです。この迷宮から脱出しましょう");
+        }else {
+            System.out.println("あなたは巨人になったうちの一匹です街を破壊しながらウォールマリアを壊しましょう");
+        }
+
+        while(!map.goal){
+            player1.MovePlayer(map);
+            System.out.print("\n\n\n\n\n\n\n");
+            map.PrintMap();
+        }
+        if(player1.wallbreak == false){
+            System.out.print("おめでとう君は迷宮から脱出することができたよ");
+        }else {
+            System.out.println("その日人類は思い出した。");
+            System.out.println("ヤツらに支配されていた恐怖を...");
+            System.out.println("鳥籠の中に囚われていた屈辱を...");
+        }
+
 
     }
 }
